8b9e0256f11c77fe31948a82461a431653624ebc
Derek Verduijn <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2026-04-13 11:28:45 +0000

    pplsssss

--- example/init.sh
+++ example/init.sh
@@ -9,11 +9,14 @@ mkdir customRoutesDir;
 
 declare -a arr=("deno.json" "kuusi.config.ts" "my.env" "myRequired.env" "customRoutesDir/:id.source.ts" "customRoutesDir/dotenv.source.ts" "customRoutesDir/index.source.ts" "customRoutesDir/kuusi.source.ts" "customRoutesDir/subscribe.hook.ts")
 
-for i in arr
+for i in "${arr[@]}"
 do 
   touch $i;
   curl https://raw.githubusercontent.com/kuusi-ts/kuusi/refs/heads/main/example/$i >> $i;
 done
 
-deno install;
+echo "To get started, follow these steps";
+echo "  1. Cd into the directory with `cd kuusiApp`";
+echo "  2. Run `deno install`;
+
 cd ../;
