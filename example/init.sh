ed3ce04eb61e476ad04cd280f98e6f1ebb1fbec3
Derek Verduijn <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2026-04-13 11:38:45 +0000

    i fucking hate bash

--- example/init.sh
+++ example/init.sh
@@ -2,21 +2,19 @@ echo "Initializing kuusi example project";
 echo "I know this script is bad, I suck at bash. Kuusi is good tho I pinky promise.";
 
 mkdir kuusiApp;
-cd kuusiApp;
 
-mkdir src;
-mkdir customRoutesDir;
+mkdir kuusiApp/src;
+mkdir kuusiApp/customRoutesDir;
 
 declare -a arr=("deno.json" "kuusi.config.ts" "my.env" "myRequired.env" "customRoutesDir/:id.source.ts" "customRoutesDir/dotenv.source.ts" "customRoutesDir/index.source.ts" "customRoutesDir/kuusi.source.ts" "customRoutesDir/subscribe.hook.ts")
 
 for i in "${arr[@]}"
 do 
-  touch $i;
-  curl https://raw.githubusercontent.com/kuusi-ts/kuusi/refs/heads/main/example/$i >> $i;
+  touch kuusiApp/$i;
+  curl https://raw.githubusercontent.com/kuusi-ts/kuusi/refs/heads/main/example/$i >> kuusiApp/$i;
 done
 
-echo "To get started, follow these steps";
-echo "  1. Cd into the directory with \`cd kuusiApp\`";
-echo "  2. Run \`deno install\`";
-
-cd ../;
+echo "To get started, run the following commands";
+echo "  cd kuusiApp";
+echo "  deno install";
+echo "  deno run dev";
